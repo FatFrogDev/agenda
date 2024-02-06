6b4bba2531624aba476a5bbfbd622a9b5bfba951
Deyby <arizapaul987@gmail.com>
2024-02-06 11:30:36 -0500

    db script added

--- src/main/resources/db/db-script.sql
+++ src/main/resources/db/db-script.sql
@@ -6,7 +6,6 @@ use agenda;
 delimiter $$
 CREATE DEFINER=`root`@`localhost` FUNCTION `save_person`(
 	in_address varchar(70) ,
-	in_cellphone varbinary(255),
 	in_is_manager tinyint(1) ,
 	in_lastname varchar(20) ,
 	in_name varchar(20) ) RETURNS tinyint(1)
@@ -17,9 +16,25 @@ begin
 	if is_found is not null then
 		return false;
 	else
-		insert into person (`address`, `cellphone`, `is_manager`, `lastname`, `name`)
-			values (in_address, in_cellphone, in_is_manager, in_lastname, in_name);
+		insert into person (`address`, `is_manager`, `lastname`, `name`) 
+			values (in_address, in_is_manager, in_lastname, in_name);
 		return  true;
+	end if;
+end $$
+delimiter ;
+
+delimiter $$
+CREATE DEFINER=`root`@`localhost` FUNCTION `check_phone_exists`(
+	in_phone varchar(15)
+    ) RETURNS tinyint(1)
+    DETERMINISTIC
+begin
+	declare is_found int;
+	select id from cellphone where `number`=in_phone limit 1 into is_found ;
+	if is_found is not null then
+		return true;
+	else
+		return false;
 	end if;
 end $$
 delimiter ;
