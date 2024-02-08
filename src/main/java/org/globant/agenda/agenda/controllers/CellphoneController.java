53d5b8acda043cafd3c3f76d1999bc1155bad576
Andres Felipe Yate <102778610+BgYato@users.noreply.github.com>
2024-02-08 14:55:31 -0500

    Set the edit cellphones

--- src/main/java/org/globant/agenda/agenda/controllers/CellphoneController.java
+++ src/main/java/org/globant/agenda/agenda/controllers/CellphoneController.java
@@ -3,18 +3,23 @@ package org.globant.agenda.agenda.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.micrometer.common.lang.NonNull;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 import java.util.Optional;
 
+import org.globant.agenda.agenda.exceptions.ResourceNotFoundException;
 import org.globant.agenda.agenda.model.Cellphone;
+import org.globant.agenda.agenda.model.Organization;
 import org.globant.agenda.agenda.repository.CellphoneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -45,5 +50,20 @@ public class CellphoneController {
         } else {
             return ResponseEntity.ok(cellphone);
         }
+    }
+
+    @SuppressWarnings("null")
+    @PutMapping("/cellphone/{id}")
+    public ResponseEntity<Cellphone> updateCellphone(@PathVariable @NonNull Integer id, @RequestBody Cellphone cellphoneUpdate) {
+        Cellphone existingCellphone = cellphoneRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("La organización con el siguiente ID no existe: " + id));
+
+        // Update the atributes necessaries without the "Person"
+        existingCellphone.setNumber(cellphoneUpdate.getNumber());
+
+        // Save the entity updated
+        Cellphone cellphoneUpdated = cellphoneRepository.save(existingCellphone);        
+
+        return ResponseEntity.ok(cellphoneUpdated);
     }
 }
